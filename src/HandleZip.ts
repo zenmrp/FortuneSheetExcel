fb35ecf3c425630183d8a49053bd0401a08a50cb
Val Packett <val@packett.cool>
2024-02-02 02:55:25 -0300

    Refactor jszip usage to async

--- src/HandleZip.ts
+++ src/HandleZip.ts
@@ -1,63 +1,44 @@
 import JSZip from "jszip";
-import {IuploadfileList} from "./ICommon.js";
+import { IuploadfileList } from "./ICommon.js";
 
+export class HandleZip {
+  uploadFile: File;
+  workBook: JSZip;
 
-export class HandleZip{
-    uploadFile:File; 
-    workBook:JSZip; 
-    
-    constructor(file?:File){
-        // Support nodejs fs to read files
-        // if(file instanceof File){
-            this.uploadFile = file;
-        // }
-    }
+  constructor(file: File) {
+    this.uploadFile = file;
+  }
 
-    unzipFile(successFunc:(file:IuploadfileList)=>void, errorFunc:(err:Error)=>void):void { 
-        // var new_zip:JSZip = new JSZip();
-        JSZip.loadAsync(this.uploadFile)                                   // 1) read the Blob
-        .then(function(zip:any) {
-            let fileList:IuploadfileList = <IuploadfileList>{}, lastIndex:number = Object.keys(zip.files).length, index:number=0;
-            zip.forEach(function (relativePath:any, zipEntry:any) {  // 2) print entries
-                let fileName = zipEntry.name;
-                let fileNameArr = fileName.split(".");
-                let suffix = fileNameArr[fileNameArr.length-1].toLowerCase();
-                let fileType = "string";
-                if(suffix in {"png":1, "jpeg":1, "jpg":1, "gif":1,"bmp":1,"tif":1,"webp":1,}){
-                    fileType = "base64";
-                }
-                else if(suffix=="emf"){
-                    fileType = "arraybuffer";
-                }
-                zipEntry.async(fileType).then(function (data:string) {
-                    if(fileType=="base64"){
-                        data = "data:image/"+ suffix +";base64," + data;
-                    }
-                    fileList[zipEntry.name] = data;
-                    // console.log(lastIndex, index);
-                    if(lastIndex==index+1){
-                        successFunc(fileList);
-                    }
-                    index++;
-                });
-            });
-            
-        }, function (e:Error) {
-            errorFunc(e);
-        });
-    }
-
-    newZipFile():void { 
-        var zip = new JSZip();
-        this.workBook =  zip;
-    }
-
-    //title:"nested/hello.txt", content:"Hello Worldasdfasfasdfasfasfasfasfasdfas"
-    addToZipFile(title:string,content:string):void { 
-        if(this.workBook==null){
-            var zip = new JSZip();
-            this.workBook =  zip;
-        }
-        this.workBook.file(title, content);
-    }
+  async unzipFile(): Promise<IuploadfileList> {
+    const zip = await JSZip.loadAsync(this.uploadFile);
+    const fileList: IuploadfileList = <IuploadfileList> {};
+    for (const [_path, zipEntry] of Object.entries(zip.files)) {
+      const fileName = zipEntry.name;
+      const fileNameArr = fileName.split(".");
+      const suffix = fileNameArr[fileNameArr.length - 1].toLowerCase();
+      let fileType: JSZip.OutputType = "string";
+      if (
+        suffix in
+          {
+            "png": 1,
+            "jpeg": 1,
+            "jpg": 1,
+            "gif": 1,
+            "bmp": 1,
+            "tif": 1,
+            "webp": 1,
+          }
+      ) {
+        fileType = "base64";
+      } else if (suffix == "emf") {
+        fileType = "arraybuffer";
+      }
+      let data = await zipEntry.async(fileType);
+      if (fileType == "base64") {
+        data = "data:image/" + suffix + ";base64," + data;
+      }
+      fileList[zipEntry.name] = data as string; // XXX: ignore EMF ArrayBuffer :/
+    };
+    return fileList;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { LuckyFile } from "./ToLuckySheet/LuckyFile.js";
 import { HandleZip } from "./HandleZip.js";
 
-import { IuploadfileList } from "./ICommon.js";
-
 export class LuckyExcel {
-  static transformExcelToLucky(excelFile: File): Promise<LuckyFile> {
-    const handleZip = new HandleZip(excelFile);
-    return new Promise((resolve, reject) =>
-      handleZip.unzipFile((files: IuploadfileList) => {
-        const luckyFile = new LuckyFile(files, excelFile.name);
-        luckyFile.Parse();
-        resolve(luckyFile);
-      }, reject)
-    );
+  static async transformExcelToLucky(excelFile: File): Promise<LuckyFile> {
+    const files = await new HandleZip(excelFile).unzipFile();
+    const luckyFile = new LuckyFile(files, excelFile.name);
+    luckyFile.Parse();
+    return luckyFile;
   }
 }
