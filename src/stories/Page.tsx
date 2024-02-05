b77b1bddf60e0a832c8d5a32f497f884d6956daf
Val Packett <val@packett.cool>
2024-02-05 00:21:48 -0300

    Add workaround for initial rendering of formatted cells

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -28,7 +28,21 @@ export const Page: React.FC = () => {
           console.log('Loaded', lsh, 'into', sheetRef) // Log the dynamic object that will be changed by the engine
         }}/>
       </header>
-      <Workbook key={key} ref={sheetRef} data={sheets} style={{ flex: '1' }} />
+      <Workbook key={key} ref={sheetRef} data={sheets} style={{ flex: '1' }} onChange={() => {
+        // XXX: Unfortunately FortuneSheet does not (yet) apply the format when we initialize it with values
+        //      so we have to force it to do it for every cell that has a non-string format to apply that.
+        //      (It also doesn't export its formatting library so we'd have to copy-paste it if we wanted
+        //       to generate the 'm' pre-cached text values ourselves.)
+        //      https://github.com/ruilisi/fortune-sheet/issues/504 (remove this handler when that gets fixed)
+        // This handler is run when the sheets themselves change, *not* any piece of data inside them.
+        for (const sheet of sheetRef.current.getAllSheets()) {
+          if (!sheet.data) continue;
+          for (let r = 0; r < sheet.data.length; r++)
+            for (let c = 0; c < sheet.data[r].length; c++)
+                if (typeof sheet.data[r][c]?.ct === 'object' && sheet.data[r][c].ct.t !== 's')
+                  sheetRef.current.setCellFormat(r, c, 'ct', sheet.data[r][c].ct, { id: sheet.id });
+        }
+      }} />
     </div>
   );
 };
